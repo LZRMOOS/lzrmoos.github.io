a561e0127efda6d97c50105362d9e717c2e58097
Wei Dai <weid@dropbox.com>
2022-09-04 22:14:07 -0700

    Update prod path

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,14 @@
 //   transpileDependencies: true
 // })
 
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//   ? '/vuejstest/'
+//   : '/'
+// };
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/vuejstest/'
+  ? '/'
   : '/'
 };
