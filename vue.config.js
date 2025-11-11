c8055eb300a69d28404c0e1a0e2f9927668c3e49
Wei Dai <weid@dropbox.com>
2025-11-11 11:35:56 -0800

    Fix SPA redirects and mobile navigation

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,14 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
+const { defineConfig } = require('@vue/cli-service')
 
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production'
-//   ? '/vuejstest/'
-//   : '/'
-// };
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/'
-  : '/'
-};
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  
+  // Ensure 404.html and other public files are copied
+  chainWebpack: config => {
+    // The public folder files are automatically copied, but we ensure it here
+    config.plugin('copy').tap(args => {
+      return args
+    })
+  }
+});
